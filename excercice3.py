c3fba51b7f15156727dde965125b8c3880722e66
ararou-israe <israeararou04@gmail.com>
2026-02-28 21:21:01 +0000

    suprimmer

--- excercice3.py
+++
@@ -1,8 +0,0 @@
-password="python123"
-user_password=input("entrer le mot de passe:")
-
-while user_password!=password:
-    print("mot de passe incorrect, essayez à nouveau.")
-    user_password=input("entrer le mot de passe:")
-print("mot de passe correct, bienvenue!")
-    
